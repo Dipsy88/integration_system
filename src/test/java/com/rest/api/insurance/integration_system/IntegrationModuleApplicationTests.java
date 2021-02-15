68128935ad9c51896426126d34c756627999caf8
dipsy88 <dipsy.urfriend@gmail.com>
2021-02-15 17:09:30 +0100

    Added project skeleton

---
+++ src/test/java/com/rest/api/insurance/integration_system/IntegrationModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rest.api.insurance.integration_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntegrationModuleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
